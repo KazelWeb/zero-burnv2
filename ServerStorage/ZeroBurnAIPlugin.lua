87bdf8e0dcbd19d54c9123943cc42664da400f1b
KazelWeb <167887006+KazelWeb@users.noreply.github.com>
2026-06-22 08:26:40 +0800

    Create ZeroBurnAIPlugin.lua

---
+++ ServerStorage/ZeroBurnAIPlugin.lua
@@ -0,0 +1,7 @@
+-- Location: ServerStorage/ZeroBurnAIPlugin
+
+local HttpService = game:GetService("HttpService")
+local RunService = game:GetService("RunService")
+local ZBGuiObjects = require(script:WaitForChild("ZBGuiObjects"))
+
+if not plugin then return end
