0b2ba80211eaf5fd1bacedfa5160aa0ab621e808
KazelWeb <167887006+KazelWeb@users.noreply.github.com>
2026-06-22 17:48:18 +0800

    Color Palette asking

--- ServerStorage/ZeroBurnAIPlugin.lua
+++ ServerStorage/ZeroBurnAIPlugin.lua
@@ -3,5 +3,6 @@
 local HttpService = game:GetService("HttpService")
 local RunService = game:GetService("RunService")
 local ZBGuiObjects = require(script:WaitForChild("ZBGuiObjects"))
+local ZBColorMap = require(script:WaitForChild("ZBColorMap"))
 
 if not plugin then return end
