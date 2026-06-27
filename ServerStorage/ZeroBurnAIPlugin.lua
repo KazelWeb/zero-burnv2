df0ae02c68db4e9a9a9fe0bba6f2146f10c3001a
KazelWeb <167887006+KazelWeb@users.noreply.github.com>
2026-06-27 09:51:34 +0800

    a

--- ServerStorage/ZeroBurnAIPlugin.lua
+++
@@ -1,9 +0,0 @@
--- Location: ServerStorage/ZeroBurnAIPlugin
-
-local HttpService = game:GetService("HttpService")
-local RunService = game:GetService("RunService")
-local ZBGuiObjects = require(script:WaitForChild("ZBGuiObjects"))
-local ZBColorMap = require(script:WaitForChild("ZBColorMap"))
-local ZBActions = require(script:WaitForChild("ZBActions"))
-
-if not plugin then return end
